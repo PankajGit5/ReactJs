a04c971b60fab06b25f9a2a6b76606b7e0aa7c5c
pankaj <pankaj@qpxtra.com>
2024-08-26 15:25:46 +0530

    Setup react

---
+++ App.js
@@ -0,0 +1,7 @@
+// const heading = React.createElement("hi",{},"hello");
+// const root = ReactDOM.createElement(document.getElementById("maindiv"));
+// root.render(heading);
+
+const heading = React.createElement("h1", {}, "hello");
+const root = ReactDOM.createRoot(document.getElementById("maindiv"));
+root.render(heading);
